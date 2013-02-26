a7bab3d1688c5c71add23e5575e7dd11469b97c4
Osman Siddique <osmansiddique@Osmans-MacBook-Air.local>
2013-02-25 18:18:33 -0600

    updated architecture

--- sstream.py
+++ sstream.py
@@ -1,19 +1,18 @@
 import wx
 import wx.grid
-import time
-import datetime
-import thread
-import ystockquote
-import chart_prices
-import matplotlib.pyplot as plt
 
-class SStream(wx.Frame):   
+import date_time_thread
+import quote_handler_thread
+import plot_stock_history
+
+# GUI frame
+class MainFrame(wx.Frame):
     def __init__(self):
-        wx.Frame.__init__(self, None, title="sstream",size=(800,500))    
-        self.panel = wx.Panel(self)  
+        wx.Frame.__init__(self, None, title="sstream",size=(825,400))
+        self.panel = wx.Panel(self)
 
         # text box for entering stock symbols
-        self.txt_box = wx.TextCtrl(self.panel)
+        self.enter_symbol_box = wx.TextCtrl(self.panel)
         
         # 'Add' button to add stock data to a grid
         add_btn = wx.Button(self.panel, label='Add')
@@ -28,16 +27,17 @@ class SStream(wx.Frame):
         # grid for displaying data
         self.newest_row = 1
         self.grid = wx.grid.Grid(self.panel)
-        self.grid.CreateGrid(self.newest_row,8)
+        self.grid.CreateGrid(self.newest_row,9)
         self.grid.SetColLabelValue(0, "Symbol")
         self.grid.SetColLabelValue(1, "Last")
         self.grid.SetColLabelValue(2, "Net Chg")
-        self.grid.SetColLabelValue(3, "Bid")
-        self.grid.SetColLabelValue(4, "Ask")
-        self.grid.SetColLabelValue(5, "Vol")
-        self.grid.SetColLabelValue(6, "Open")
-        self.grid.SetColLabelValue(7, "Prev Close")
-        self.Bind(wx.grid.EVT_GRID_CELL_LEFT_CLICK, self.ChartHistoricPrices)
+        self.grid.SetColLabelValue(3, "Exchange")
+        self.grid.SetColLabelValue(4, "Bid")
+        self.grid.SetColLabelValue(5, "Ask")
+        self.grid.SetColLabelValue(6, "Vol")
+        self.grid.SetColLabelValue(7, "Open")
+        self.grid.SetColLabelValue(8, "Prev Close")
+        self.Bind(wx.grid.EVT_GRID_CELL_LEFT_CLICK, self.OnSymbolClick)
         
         # layout the gui
         symbol_input_sizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -45,7 +45,7 @@ class SStream(wx.Frame):
         grid_sizer = wx.BoxSizer(wx.HORIZONTAL)
         main_sizer = wx.BoxSizer(wx.VERTICAL)
         # sizer for input
-        symbol_input_sizer.Add(self.txt_box, 0, wx.ALL, 5)
+        symbol_input_sizer.Add(self.enter_symbol_box, 0, wx.ALL, 5)
         symbol_input_sizer.Add(add_btn, 0, wx.ALL, 5)
         # sizer for date and time
         date_time_sizer.Add(self.time_display, 0, wx.ALL, 5)
@@ -55,94 +55,65 @@ class SStream(wx.Frame):
         # sizer for entire gui
         main_sizer.Add(symbol_input_sizer, 0, wx.ALL, 5)
         main_sizer.Add(date_time_sizer, 0, wx.ALL, 5)
-        main_sizer.Add(grid_sizer, 1, wx.ALL, 5)   
+        main_sizer.Add(grid_sizer, 1, wx.ALL, 5)
         self.panel.SetSizer(main_sizer)
         
         # start a thread to handle date/time updates
-        thread.start_new_thread(self.DateTimeHandler,())
+        date_time_thread.DateTimeThread(self)
         
         # start getting quotes for AAPL by default
-        thread.start_new_thread(self.QuoteHandler,('AAPL',0))
+        self.symbol_set = {"AAPL"}
+        quote_handler_thread.QuoteHandlerThread(self,'AAPL',0)
        
-    def OnAdd(self,event): 
-        # add a row to grid
-        self.grid.AppendRows(1)
-        self.newest_row += 1   
-        # start a thread
-        thread.start_new_thread(self.QuoteHandler,(self.txt_box.GetValue().upper(),self.newest_row-1)) 
+    def OnAdd(self,event):
+        symbol = self.enter_symbol_box.GetValue().upper()
+        # check to see if already monitoring that stock
+        if symbol not in self.symbol_set:
+            # add a row to grid
+            self.grid.AppendRows(1)
+            self.newest_row += 1
+            # add the symbol to the set
+            self.symbol_set.add(symbol)
+            # start a new thread
+            quote_handler_thread.QuoteHandlerThread(self,symbol,self.newest_row-1)
         # clear the text box
-        self.txt_box.Clear()
-        self.txt_box.SetFocus()
-        
-    def QuoteHandler(self,symbol,row):
-        self.grid.SetCellValue(row, 0, symbol)
-        # get initial price
-        quote = float(ystockquote.get_price(symbol))
-        set_cell_color = lambda x,y: (x>y and ("Green",True)) or (x<y and ("Red",True)) or (("White",False))
-        while 1:
-            # get and set values
-            stock_data = ystockquote.get_all(symbol)
-            prev_quote = quote 
-            quote = stock_data['price']
-            prev_close = float(quote) - float(stock_data['change'])
-            # determine cell colors
-            quote_color = set_cell_color(float(quote),float(prev_quote))[0]
-            net_change_color = set_cell_color(float(stock_data['change']),0.0)[0]
-            self.UpdateStockDataDisplay(row,\
-                                        quote,\
-                                        quote_color,\
-                                        stock_data['change'],\
-                                        net_change_color,\
-                                        stock_data['volume'],\
-                                        str(prev_close))
-            # wait 3 seconds
-            time.sleep(1)
-            self.grid.SetCellBackgroundColour(row,1,"White")
-            self.grid.ForceRefresh()
-            time.sleep(2)
+        self.enter_symbol_box.Clear()
+        self.enter_symbol_box.SetFocus()
     
-    def UpdateStockDataDisplay(self,row,quote,quote_color,change,change_color,volume,prev):
+    def OnSymbolClick(self, event):
+         if event.GetCol() == 0:
+            if plot_stock_history.PlotCurrentlyDisplayed():
+               wx.MessageBox('Only one plot at a time!', 'Warning', wx.OK|wx.ICON_INFORMATION)
+            else:
+              plot_stock_history.PlotClosingPrices(self.grid.GetCellValue(event.GetRow(),0))
+         self.enter_symbol_box.SetFocus()
+        
+    def DisplayTickerSymbol(self,row,symbol):
+        self.grid.SetCellValue(row, 0, symbol)
+    
+    def UpdateStockDataDisplay(self,row,quote,quote_color,change,change_color,exchange,volume,prev):
         self.grid.SetCellValue(row,1,quote)
         self.grid.SetCellBackgroundColour(row,1,quote_color)
         self.grid.SetCellValue(row,2,change)
         self.grid.SetCellBackgroundColour(row,2,change_color)
-        self.grid.SetCellValue(row,5,volume)
-        self.grid.SetCellValue(row,7,prev)
-    
-    def DateTimeHandler(self):
-        format_time = lambda x, c: (x < 10 and (c + str(x), True)) or (str(x), False)
-        while 1:
-           # set time values
-           now = datetime.datetime.now()
-           str_hour = format_time(now.hour,' ')[0]
-           str_minute = format_time(now.minute,'0')[0]
-           str_second = format_time(now.second,'0')[0]
-           am_pm = ((now.hour >= 12 and ('PM',True)) or ('AM',False))[0]
-           # update the date and time
-           self.UpdateDateTimeDisplay(str_hour,\
-                                      str_minute,\
-                                      str_second,\
-                                      am_pm,\
-                                      str(datetime.date.today()))
-           # wait   
-           time.sleep(1)       
+        self.grid.SetCellValue(row,3,exchange)
+        self.grid.SetCellValue(row,6,volume)
+        self.grid.SetCellValue(row,8,prev)
+        
+    def SetQuoteCellColorWhite(self,row):
+        self.grid.SetCellBackgroundColour(row,1,"White")
+        self.grid.ForceRefresh()
     
     def UpdateDateTimeDisplay(self,hour,minute,second,am_pm,date):
-         self.time_display.SetValue(hour + ':' + minute + ':' + second + ':' + am_pm)
-         self.date_display.SetValue(date)
-    
-    def ChartHistoricPrices(self, event):
-         if event.GetCol() == 0:
-            print 'hi'
-            if plt.fignum_exists(1):
-               wx.MessageBox('Only one plot at a time!', 'Warning', wx.OK|wx.ICON_INFORMATION)
-            else:
-              symbol = self.grid.GetCellValue(event.GetRow(),0) 
-              chart_prices.ChartPrice(symbol)
-         self.txt_box.SetFocus()
- 
+        self.time_display.SetValue(hour + ':' + minute + ':' + second + ':' + am_pm)
+        self.date_display.SetValue(date)
+
+class SStream(wx.App):
+    def OnInit(self):
+        self.frame = MainFrame()
+        self.frame.Show(True)
+        return True
+           
 if __name__ == '__main__':
-    app = wx.App()
-    frame = SStream()
-    frame.Show()
+    app = SStream(0)
     app.MainLoop()
